ebd8911be7fe9e68b8c760f410b0a311ff72a89d
Aliev Damir <damir-aliev-2012@mail.ru>
2022-04-25 15:37:05 +0500

    first commit

---
+++ CauchyProblemNumericMethods.cpp
@@ -0,0 +1,87 @@
+﻿#include <iostream>
+
+double func(double const& x, double const& y)
+{
+	return (-2) * x * (y + 1);
+}
+
+void EulerMethod(size_t const& n, double x, double y, double const& step)
+{
+	std::cout << "Euler Method:\n\n";
+
+	for (size_t i = 0; i <= n; ++i)
+	{
+		std::cout << 'x' << i << ": " << x << "    \t" << 'y' << i << ": " << y << '\n';
+
+		y += step * func(x, y);
+		x += step;
+	}
+
+	std::cout << '\n';
+}
+
+void CauchyMethod(size_t const& n, double x, double y, double const& step)
+{
+	std::cout << "Cauchy Method:\n\n";
+
+	for (size_t i = 0; i <= n; ++i)
+	{
+		std::cout << 'x' << i << ": " << x << "    \t" << 'y' << i << ": " << y << '\n';
+
+		y += step * func(x + step/2, y + (step/2) * func(x, y));
+		x += step;
+	}
+
+	std::cout << '\n';
+}
+
+void RungeKutta4th(double& x, double Y[], double const& step)
+{
+    double k1, k2, k3, k4;
+    
+    for (size_t i = 1; i < 3; ++i)
+    {
+		k1 = step * func(x, Y[i - 1]);
+		k2 = step * func(x + step / 2, Y[i - 1] + k1 / 2);
+		k3 = step * func(x + step / 2, Y[i - 1] + k2 / 2);
+		k4 = step * func(x + step, Y[i - 1] + k3);
+
+		Y[i] = Y[i - 1] + 1./ 6 * (k1 + 2 * k2 + 2 * k3 + k4);
+
+		x += step;
+
+        std::cout << 'x' << i << ": " << x << "    \t" << 'y' << i << ": " << Y[i] << '\n';
+    }
+}
+
+void AdamsBashforthMethod(size_t const& n, double x, double const& y, double const& step)
+{
+
+	std::cout << "Adams-Bashforth Method:\n\n";
+
+    std::cout << "x0: " << x << "    \t" << "y0: " << y << '\n';
+
+	double* Y = new double[n + 1];
+	Y[0] = y;
+
+    RungeKutta4th(x, Y, step);
+    
+	for (size_t i = 3; i <= n; ++i)
+    {
+        Y[i] = Y[i - 1] + (step / 12) * (23 * func(x, Y[i - 1]) - 16 * func(x - step, Y[i - 2]) + 5 * func(x - 2 * step, Y[i - 3]));\
+
+        x += step;
+
+		std::cout << 'x' << i << ": " << x << "    \t" << 'y' << i << ": " << Y[i] << '\n';
+    }
+
+    std::cout << '\n';
+}
+
+int main()
+{
+	EulerMethod(10, 0, 1, 0.1);
+	CauchyMethod(10, 0, 1, 0.1);
+	AdamsBashforthMethod(10, 0, 1, 0.1);
+}
+
---
+++ IntegrationNumericMethods.cpp
@@ -0,0 +1,67 @@
+﻿#include<cmath>
+#include<iostream>
+
+/*---integrand---*/
+double function(double x)
+{
+	return sin(x*x);
+}
+
+/*---right rectangle method---*/
+double rightRectangle(double left, double right, double step)
+{
+	double sum = 0;
+	double runner;
+
+	for (runner = left + step; runner <= right; runner += step)
+		sum += function(runner);
+
+	sum *= step;
+
+	return sum;
+}
+
+/*---trapezium method---*/
+double trapezium(double left, double right, double step)
+{
+	double sum = 0;
+	double runner;
+
+	for (runner = left + step; runner < right; runner += step)
+		sum += function(runner);
+
+	sum = (sum + 0.5 * (function(left) + function(right))) * step;
+
+	return sum;
+}
+
+int main()
+{
+	double a, b, step;
+
+	std::cout << "Enter lower limit of integration:\n";
+	std::cin >> a;
+
+	std::cout << "Enter upper limit of integration:\n";
+	std::cin >> b;
+
+	std::cout << "Enter integration step:\n";
+	std::cin >> step;
+
+	std::cout << '\n';
+
+	double rect = rightRectangle(a, b, step);
+	double trap = trapezium(a, b, step);
+
+	double divRect = rightRectangle(a, b, step / 2);
+	double divTrap = trapezium(a, b, step / 2);
+
+	double rectError = (rect - divRect) / 2; //Runge's error estimation rule
+	double trapError = (trap - divTrap) / 2;
+
+	std::cout << "Value by right rectangles method is " << rect << '\n';
+	std::cout << "Error: " << rectError << "\n\n";
+
+	std::cout << "Value by trapezium method is " << trap << '\n';
+	std::cout << "Error: " << trapError << "\n\n";
+}
